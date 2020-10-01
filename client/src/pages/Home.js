65f46c27d0e58acee91e83e5669a6123a7e9120a
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-10-01 16:27:59 -0400

    created home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Jumbotron} from 'react-bootstrap';
+
+class Home extends Component {
+    render() {
+        return (
+            <section id="home">
+               
+                
+            </section>
+        );
+    }
+}
+
+export default Home;
