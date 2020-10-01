86327566e4528d2acaf9e3923601b0f699a8c307
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-10-01 16:27:49 -0400

    created jumbotron page

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap';
+class Jumbotron extends Component {
+    render() {
+        return (
+           <Jumbotron>
+               
+           </Jumbotron>
+        );
+    }
+}
+
+export default Jumbotron;
