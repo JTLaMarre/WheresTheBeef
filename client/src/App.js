86e5ee4215370733db7026d147ca553beb4bd5ef
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-10-01 23:56:35 -0400

    imported navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import * as ReactBootStrap from "react-bootstrap";
+import NavBar from './components/Nav';
 import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+    <Router>
+        <NavBar />
+ 
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/" >
+              <Home/>
+          </Route>
+          
+        </Switch>
+    </Router>
     </div>
   );
 }
