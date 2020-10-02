be006540af6b863bcf978893a748c966d39a985f
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-10-01 21:04:35 -0400

    added Nav to home page- will move it to app page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Nav from '../components/Nav';
 import Hero from '../components/Jumbotron/Jumbotron';
 
 class Home extends Component {
     render() {
         return (
             <section id="home">
+                <Nav/>
                <Hero/>
                 
             </section>
